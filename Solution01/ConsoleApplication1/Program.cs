88179ad210dd9c8d2184c269a7ade5b8b1a9e8ad
shtigran <shakhbekyantigran@gmail.com>
2016-12-12 16:44:19 +0400

    First part

--- Solution01/ConsoleApplication1/Program.cs
+++ Solution01/ConsoleApplication1/Program.cs
@@ -15,7 +15,9 @@ namespace ConsoleApplication1
             var test = new Test();
 
             test.DoWork();
-           
+            test.DoWork();
+            test.DoWork();
+
         }
     }
 }
