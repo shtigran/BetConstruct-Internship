943c09dab15e41e64fdf7239c0eb40a03e21a9d3
shtigran <shakhbekyantigran@gmail.com>
2016-12-12 15:42:26 +0400

    Added ClassLibrary and ConsoleAplication

---
+++ Solution01/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Test();
+
+        }
+    }
+}
