b52c5a733ffe71ac24bf3a0c56a3b4f30accd4e2
Tigran <Tigran@TIGRAN->
2017-01-09 20:47:52 +0400

    123

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,40 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello");
+            Console.WriteLine("__________Hi Armine OLEYAN__________");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Mtqumd mi hat tiv pahi");
+
+            Console.WriteLine();
+            Console.WriteLine("Ayd tiv@ bazmapatki erkusov");
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Gumari erku");
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hani qo pahac tiv@");
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Gumari 20");
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hani 8");
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hani qo pahac tiv@");
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Bazmapatki 4-ov");
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Lav parz e");
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("hima mtqid tiv@ 56 -n e!!!");
+
+            Console.ReadKey();
         }
     }
 }
