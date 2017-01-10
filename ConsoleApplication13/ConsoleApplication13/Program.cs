1fc057de53f54240f63a219d1058c7ffb2b5cb3f
shtigran <shakhbekyantigran@gmail.com>
2017-01-10 16:22:49 +0400

    222

--- ConsoleApplication13/ConsoleApplication13/Program.cs
+++ ConsoleApplication13/ConsoleApplication13/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication13
         static void Main(string[] args)
         {
             Console.WriteLine("ssdf ");
+            Console.WriteLine("ssdf ");
         }
     }
 }
