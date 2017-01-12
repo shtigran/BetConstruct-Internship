25d9f58fc3cf2166f2fad42cb805ca60882c34e9
shtigran <shakhbekyantigran@gmail.com>
2017-01-13 01:10:07 +0400

    Classes in the different files

--- PasswordGenerator/PasswordGenerator/PasswordGenerator/PasswordGenCrypto.cs
+++ PasswordGenerator/PasswordGenerator/PasswordGenerator/PasswordGenCrypto.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
+ 
 
 namespace PasswordGenerator
 {
--- PasswordGenerator/PasswordGenerator/Program/Program.cs
+++ PasswordGenerator/PasswordGenerator/Program/Program.cs
@@ -12,19 +12,19 @@ namespace Program
         static void Main(string[] args)
         {
             
-            Console.WriteLine("The password generation by: RNGCrypto.");
+            Console.WriteLine("The password generation by: PasswordGenCrypto.");
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine(PasswordGenCrypto.PassGen(16));
-            }          
-
-            Console.WriteLine("The password generation by: RNGCrypto and BitConverter.");
+            }
+            Console.WriteLine();
+            Console.WriteLine("The password generation by: PasswordGenCryptoBit.");
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine(PasswordGenCryptoBit.PassGen(32));
             }
-
-            Console.WriteLine("The password generation by: Random.");
+            Console.WriteLine();
+            Console.WriteLine("The password generation by: PasswordGenRandom.");
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine(PasswordGenRandom.PassGenRand(32));
