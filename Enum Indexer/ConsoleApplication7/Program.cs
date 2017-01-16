8920a19379ebfc56db261ee12b8be6664cc4fbf9
shtigran <shakhbekyantigran@gmail.com>
2017-01-16 20:38:07 +0400

    Enum Indexer example

---
+++ Enum Indexer/ConsoleApplication7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Days;
+
+namespace DaysIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EnumDays day = new EnumDays();
+            Console.WriteLine($"The Friday is the {day["Friday"]}-th day");
+            Console.WriteLine($"The Saturday is the {day["Saturday"]}-th day");
+            Console.WriteLine($"The Monday is the {day["Monday"]}-th day");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Enum Indexer/Days/EnumDays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Days
+{
+    public class EnumDays
+    {
+        public int this [string day]
+        {
+
+            get
+            {
+                if (day == DayOfWeek.Monday.ToString() || day == DayOfWeek.Friday.ToString() 
+                    || day == DayOfWeek.Saturday.ToString() || day == DayOfWeek.Sunday.ToString() 
+                    || day == DayOfWeek.Thursday.ToString() || day == DayOfWeek.Tuesday.ToString() 
+                    || day == DayOfWeek.Wednesday.ToString())
+                    return (int)(DayOfWeek)Enum.Parse(typeof(DayOfWeek), day);
+                else throw new Exception("The string is not day of week");
+            }
+        }
+
+    }
+}
