64c9e1c74744526d6d7b9cb67f90edd52dc1f0a3
shtigran <shakhbekyantigran@gmail.com>
2017-01-16 23:23:08 +0400

    Extension methods for string

---
+++ ExtensionMethods/ExtensionMethods/ForStrings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class ForStrings
+    {
+
+        public static bool HasLetter (this string str)
+        {
+            for (int i = 0; i <str.Length; i++)
+            {
+               if(char.IsLetter(str[i]))
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool HasNumber (this string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsNumber(str[i]))
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool HasPunctuation (this string str)
+        {
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsPunctuation(str[i]))
+                    return true;
+            }
+            return false;
+        }
+
+        public static string ConCat (this string str, string str1)
+        {
+            string result = str + str1;
+            return result;
+        }
+             
+    }
+}
---
+++ ExtensionMethods/Program/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethods;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "1233424,5";
+            string str1 = "Only text";
+
+            Console.WriteLine(str.HasLetter());
+            Console.WriteLine(str1.HasLetter());
+            Console.WriteLine(str.HasNumber());
+            Console.WriteLine(str1.HasNumber());
+            Console.WriteLine(str.HasPunctuation());
+            Console.WriteLine(str1.HasPunctuation());
+            Console.WriteLine(str1.ConCat(str));
+
+            Console.ReadKey();
+
+        }
+    }
+}
