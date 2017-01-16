24ff5b8ed775a3bdc83ab1e3eed7427ef8d87a52
shtigran <shakhbekyantigran@gmail.com>
2017-01-17 00:17:59 +0400

    Extension method IsPolindrom

---
+++ ExtensionMethodIsPolindrom/ExtensionMethod/Polindrom.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class Polindrom
+    {
+        public static bool IsPolindrom (this int value)
+        {
+            int count = 0, temp1;
+            value = Math.Abs(value);
+            temp1 = value;
+            while (temp1 > 0)
+            {
+                count++;
+                temp1 /= 10;
+            }
+            if (count == 1)
+            {
+                return true;
+            }
+
+            int result = 0, temp2 = value;
+            for (int i = 0; i < count; i++)
+            {
+                result = result * 10 + temp2 % 10;
+                temp2 /= 10;
+            }
+            if (value == result)
+                return true;
+            else return false;
+        }
+    }
+}
---
+++ ExtensionMethodIsPolindrom/Program/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethod;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int number1 = 525;
+            Console.WriteLine(number1.IsPolindrom());
+            number1 = 0;
+            Console.WriteLine(number1.IsPolindrom());
+            number1 = 4;
+            Console.WriteLine(number1.IsPolindrom());
+            number1 = -2332;
+            Console.WriteLine(number1.IsPolindrom());
+            number1 = -1;
+            Console.WriteLine(number1.IsPolindrom());
+            number1 = 332;
+            Console.WriteLine(number1.IsPolindrom());
+            number1 = 55555;
+            Console.WriteLine(number1.IsPolindrom());
+            number1 = 888;
+            Console.WriteLine(number1.IsPolindrom());
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
